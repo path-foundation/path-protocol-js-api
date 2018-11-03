c753b8d27048e7faca9690c4c0bf1bc2e46c4b75
Andrey Leybovich <andrey.leybovich@novartis.com>
2018-11-03 15:44:11 -0400

    Added PathToken API and unit tests

---
+++ src/pathtoken.js
@@ -0,0 +1,84 @@
+const truffleContract = require('truffle-contract');
+
+class PathToken {
+    constructor(web3, abi, address) {
+        let instance;
+        let initialized;
+
+        this.init = async () => {
+            if (!initialized) {
+                const contract = truffleContract({ abi });
+                contract.setProvider(web3.currentProvider);
+                instance = await contract.at(address);
+                initialized = true;
+            }
+        };
+
+        /**
+         * @description Get the balance of an address
+         * @param {*} address Address to get balance of
+         * @returns {bignumber} account balance
+         */
+        this.balanceOf = async (sender) => {
+            await this.init();
+
+            const balance = await instance.balanceOf(sender);
+            return balance;
+        };
+
+        /**
+         * @description Transfer tokens from sender account to a recipient
+         * @param {address} sender - address of token sender
+         * @param {address} receiver - address of token recipient
+         * @param {bignumber} value - number of tokens to send
+         * @returns transaction receipt
+         */
+        this.transfer = async (sender, receiver, value) => {
+            await this.init();
+
+            const tx = await instance.transfer(receiver, value, { from: sender });
+            return tx;
+        };
+
+        /**
+         * @description Approve spender to spend `value` tokens from `owner`
+         * Teh transaction has to be sent by the `owner`
+         */
+        this.approve = async (owner, spender, value) => {
+            await this.init();
+
+            const tx = await instance.approve(spender, value, { from: owner });
+            return tx;
+        };
+
+        /**
+         * @description Function to check the amount of tokens that an owner allowed to a spender.
+         * @param {address} owner - the address which owns the funds.
+         * @param {address} spender - the address which will spend the funds.
+         * @return {bignumber} the amount of tokens still available for the spender.
+         */
+        this.allowance = async (owner, spender) => {
+            await this.init();
+
+            const amount = await instance.allowance(owner, spender);
+            return amount;
+        };
+
+        /**
+         * /**
+         * @description Transfer tokens from one address to another
+         * @param {address} from - the address which you want to send tokens from
+         * @param {address} to - the address which you want to transfer to
+         * @param {bignumber} - the amount of tokens to be transferred
+         * @param spender - address that initiates the transaction
+         */
+        this.transferFrom = async (from, to, value, spender) => {
+            await this.init();
+
+            const tx = await instance.transferFrom(from, to, value, { from: spender });
+            return tx;
+        };
+    }
+}
+
+module.exports = PathToken;
---
+++ test/pathtoken.test.js
@@ -0,0 +1,104 @@
+const assert = require('assert');
+const Web3 = require('web3');
+const Resolver = require('truffle-resolver');
+const path = require('path');
+const { generateAddressesFromSeed } = require('./util/keys');
+
+const artifacts = new Resolver({
+    // Project directory
+    working_directory: path.resolve(`${__dirname}/../`),
+    // Artifacts directory
+    contracts_build_directory: path.resolve(`${__dirname}/../node_modules/path-protocol-artifacts/abi`),
+});
+
+const web3 = new Web3('http://127.0.0.1:7545');
+
+const keys = generateAddressesFromSeed(process.env.TEST_MNEMONIC, 10);
+
+const PathTokenArtifact = artifacts.require('PathToken');
+
+// issuers.js module that we are testing
+const PathToken = require('../src/pathtoken');
+
+describe('PathToken API', () => {
+    let tokenApi,
+        tokenContract;
+
+    const identities = {
+        account1: keys[0],
+        account2: keys[1],
+        account3: keys[2],
+    };
+
+    before(async () => {
+        Object.keys(identities).forEach(i => {
+            web3.eth.accounts.wallet.add(identities[i].privateKey);
+        });
+
+        PathTokenArtifact.setProvider(web3.currentProvider);
+
+        tokenContract = await PathTokenArtifact.new({ from: identities.account1.address });
+
+        console.log(`PthToken address: ${tokenContract.address}`);
+
+        // Instantiate issuers API
+        tokenApi = new PathToken(web3, tokenContract.abi, tokenContract.address);
+    });
+
+    it('transfer tokens from account1 to account2', async () => {
+        const account1balance1 = await tokenApi.balanceOf(identities.account1.address);
+        const account2balance1 = await tokenApi.balanceOf(identities.account2.address);
+
+        // we don't wanna add explicit dependency on BN because lazy.
+        const BN = account1balance1.constructor;
+
+        // transfer 125 path tokens from account1 to account 2
+        await tokenApi.transfer(identities.account1.address, identities.account2.address, 125);
+
+        const account1balance2 = await tokenApi.balanceOf(identities.account1.address);
+        const account2balance2 = await tokenApi.balanceOf(identities.account2.address);
+
+        assert.ok(account1balance1.sub(new BN(125)).eq(account1balance2));
+        assert.ok(account2balance1.add(new BN(125)).eq(account2balance2));
+    });
+
+    it('transfer tokens from account1 to account3 by account2', async () => {
+        const account1balance1 = await tokenApi.balanceOf(identities.account1.address);
+        const account2balance1 = await tokenApi.balanceOf(identities.account2.address);
+        const account3balance1 = await tokenApi.balanceOf(identities.account3.address);
+
+        console.log(account1balance1.toNumber());
+        console.log(account2balance1.toNumber());
+        console.log(account3balance1.toNumber());
+
+        const transferValue = 243;
+
+        // approve account2 to transfer money from account1
+        await tokenApi.approve(identities.account1.address, identities.account2.address, transferValue);
+
+        const allowance = await tokenApi.allowance(identities.account1.address, identities.account2.address);
+
+        // make sure the approved value has been allowed
+        assert.ok(allowance.toNumber() === transferValue, `Allowance should be ${transferValue} tokens`);
+
+        // Transfer tokens from account1 to account3 by account2
+        await tokenApi.transferFrom(identities.account1.address,
+            identities.account3.address,
+            transferValue,
+            identities.account2.address);
+
+        const account1balance2 = await tokenApi.balanceOf(identities.account1.address);
+        const account2balance2 = await tokenApi.balanceOf(identities.account2.address);
+        const account3balance2 = await tokenApi.balanceOf(identities.account3.address);
+
+        console.log(account1balance2.toNumber());
+        console.log(account2balance2.toNumber());
+        console.log(account3balance2.toNumber());
+
+        // We don't want to add explicit dependency on BN because lazy.
+        const BN = account1balance1.constructor;
+
+        assert.ok(account1balance1.sub(new BN(transferValue)).eq(account1balance2), `Balance should decrease by ${transferValue}`);
+        assert.ok(account3balance1.add(new BN(transferValue)).eq(account3balance2), `Balance should increase by ${transferValue}`);
+    });
+});
